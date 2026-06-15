5418c59e2c5b5f50e44e5b9160e32dde8c0a7dc3
Pouya <pouya3883@gmail.com>
2026-06-15 11:50:28 +0400

    feat: render dynamic color-coded polylines to illustrate transit tracks

--- dashboard/app.js
+++ dashboard/app.js
@@ -5,6 +5,49 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: "© OpenStreetMap contributors",
 }).addTo(map);
 
+const routeBlueprints = {
+  "DXB-TRUCK-001": {
+    color: "blue",
+    coords: [
+      [25.0765, 55.1504],
+      [25.0932, 55.1645],
+      [25.1154, 55.1901],
+      [25.1412, 55.2188],
+      [25.1685, 55.2455],
+      [25.1972, 55.2744],
+    ],
+  },
+  "DXB-TRUCK-002": {
+    color: "black",
+    coords: [
+      [25.132, 55.2612],
+      [25.1544, 55.275],
+      [25.1788, 55.2905],
+      [25.1911, 55.3022],
+      [25.2048, 55.2708],
+    ],
+  },
+  "DXB-TRUCK-003": {
+    color: "green",
+    coords: [
+      [25.1102, 55.214],
+      [25.1155, 55.221],
+      [25.1201, 55.2265],
+      [25.1224, 55.2301],
+    ],
+  },
+};
+
+Object.keys(routeBlueprints).forEach((truckId) => {
+  const routeInfo = routeBlueprints[truckId];
+  L.polyline(routeInfo.coords, {
+    color: routeInfo.color,
+    weight: 4,
+    opacity: 0.6,
+    dashArray: "5, 10",
+  }).addTo(map);
+});
+
 let vehicleMarkers = {};
 
 async function updateFleetPositions() {
